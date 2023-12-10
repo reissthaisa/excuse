2dbb3dff68ffe2e6409b5772c774e29f019e0391
reissthaisa <144729813+reissthaisa@users.noreply.github.com>
2023-12-10 21:06:07 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,30 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function randomNumber(limit) {
+  const mathRandomNumber = Math.random();
+  const randomInteger = Math.floor(mathRandomNumber * limit);
+  return randomInteger;
+}
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  const excuse = [
+    who[randomNumber(3)],
+    action[randomNumber(3)],
+    what[randomNumber(2)],
+    when[randomNumber(4)]
+  ].join(" ");
+  document.getElementById("excuse").innerHTML = excuse;
 };
